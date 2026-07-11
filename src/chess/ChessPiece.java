f5a9a557b932b0df30a4dfc0af3bbfe5e07dca8c
san <sandrolocal41@gmail.com>
2026-07-11 20:24:46 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,10 +16,15 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
-
+    
 	public void setColor(Color color) {
 		this.color = color;
 	}
+	
+	public ChessPosition getChessPosition()  {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	
 	protected boolean isThereOpponentPice(Position position)  {
 		ChessPiece  p  = (ChessPiece)getBoard().piece(position);
